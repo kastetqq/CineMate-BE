a9fa804960d816b2b9b7421b997cb15184e2788f
Konstantin Pashkov <kostya.pashkov.90@mail.ru>
2026-03-02 23:12:50 +0600

    fix upcoming

--- src/main/java/com/ratingapp/movie/service/MovieService.java
+++ src/main/java/com/ratingapp/movie/service/MovieService.java
@@ -30,7 +30,7 @@ public class MovieService {
     private static final int UPCOMING_MOVIES_LIMIT = 5;
     private static final int PAGES_TO_FETCH = 5;
     
-    @Scheduled(cron = "0 10 17 * * *")
+    @Scheduled(cron = "0 15 17 * * *")
     @Transactional
     public void refreshAllCategories() {
         log.info("Starting daily refresh of all movie categories");
